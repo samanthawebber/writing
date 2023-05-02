07a42ff6a5c34988266d512dfcc1233b3b1915f5
Samantha Vandor <samantha.vandor@protonmail.com>
2023-05-02 17:29:48 +0200

    delete old credentials; create new credentials via rails credentials: edit

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Blog
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.active_record.schema_format = :sql
+    config.active_support.key_generator_hash_digest_class = OpenSSL::Digest::SHA1
   end
 end
